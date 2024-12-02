7994b76b65f3d594929a21adf7e4762c9d71ffcd
MoneyGrabYT <83646887+mgytr@users.noreply.github.com>
2024-12-02 19:02:47 +0100

    update

--- script.js
+++ script.js
@@ -151,7 +151,22 @@ var commands = {
         printText('you', false)
     },
     'fastfetch': () => {
-        printText('<br><img src="/me.png" width=160, height=160 style="padding-left: 20px; margin-bottom: 20px;"></img><div style="padding-left: 190px; margin-top:-160px">OS: Arch Linux x64<br>RAM: 8 GB<br>SSD: 256 GB<br>Host: HP 250 G10<br>CPU: 13th Gen Intel Core i5-1335U 1.30GHz<br>GPU: Intel UHD Graphics 770</div><br><br><a style="background-color: #383838; color: #ffffff !important" href="https://github.com/mgytr">GitHub</a> <a style="background-color: rgb(76, 116, 217); color: #FFFFFF" onclick="execcmd(\'discord\')" href="#">Discord</a> <a style="background-color: #E3C857; color: #373737" href="https://blog.mgyt.xyz/">Blog</a><br>')
+        printText(`
+        <br>
+      /\\        OS: Arch Linux x64<br>
+     /  \\       RAM: 8 GB<br>
+    /    \\      SSD: 256 GB<br>
+   /      \\     Host: HP 250 G10<br>
+  /   ,,   \\    CPU: 13th Gen Intel Core i5-1335U 4.60GHz<br>
+ /   |  |   \\   GPU: Intel UHD Graphics 770<br>
+/_-''    ''-_\\ 
+        OS: Arch Linux x64
+        <br>RAM: 8 GB
+        <br>SSD: 256 GB
+        <br>Host: HP 250 G10
+        <br>CPU: 13th Gen Intel Core i5-1335U 1.30GHz
+        <br>GPU: Intel UHD Graphics 770</div>
+        <br><br><a style="background-color: #383838; color: #ffffff !important" href="https://github.com/mgytr">GitHub</a> <a style="background-color: rgb(76, 116, 217); color: #FFFFFF" onclick="execcmd(\'discord\')" href="#">Discord</a> <a style="background-color: #E3C857; color: #373737" href="https://blog.mgyt.xyz/">Blog</a><br>`)
     },
     'projects': () => {
         printText('<a style="background-color: rgb(56, 56, 220); color: #000000" href="https://github.com/mgytr/MangaDownloader">MangaDownloader</a> - CLI for downloading Manga to your Kindle from libgen.li<br><a style="background-color: rgb(56, 56, 220); color: #000000" href="https://github.com/mgytr/SpotifyTUI">SpotifyTUI</a> - TUI for controlling spotify using the Spotify Web API (premium required)')
